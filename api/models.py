18b4da0ab15e1012ba588d3f3e0c00bca5b68f65
Rafael <rafaelbprates@gmail.com>
2021-07-06 15:23:23 -0400

    Add biling info to database

--- api/models.py
+++ api/models.py
@@ -24,3 +24,65 @@ class Kwh(models.Model):
 
     def __str__(self):
         return f'User: {self.user} | Carga: {self.load} | Kwh: {self.kwh}'
+
+
+class BillingInfo(models.Model):
+    """
+    Tabela responsável por armazenar os dados da fatura do cliente. 
+    """
+
+    grupo_choices = [
+        ("A", "Grupo A"),
+        ("B", "Grupo B"),
+    ]
+
+    sub_grupo_choices = [
+        ("A1", "A1"),
+        ("A2", "A2"),
+        ("A3", "A3"),
+        ("A3a", "A3a"),
+        ("A4", "A4"),
+        ("AS", "AS"),
+        ("B1", "B1"),
+        ("B2", "B2"),
+        ("B3", "B3"),
+        ("B4", "B4"),
+    ]
+
+    modalidade_choices = [
+        ("Convencional", "Convencional"),
+        ("Azul", "Azul"),
+        ("Branca", "Branca"),
+        ("Verde", "Verde"),
+    ]
+
+    user = models.ForeignKey(User, blank=False, on_delete=models.CASCADE, default=None)
+    grupo = models.CharField(max_length=1, choices=grupo_choices)
+    sub_grupo = models.CharField(max_length=3, choices=sub_grupo_choices)
+    modalidade_tarifaria = models.CharField(max_length=12, choices=modalidade_choices)
+
+    def __str__(self):
+        return f"Cliente: {self.user}"
+
+
+class Student(models.Model):
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    GRADUATE = 'GR'
+    YEAR_IN_SCHOOL_CHOICES = [
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+        (GRADUATE, 'Graduate'),
+    ]
+    year_in_school = models.CharField(
+        max_length=2,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default=FRESHMAN,
+    )
+
+    def is_upperclass(self):
+        return self.year_in_school in {self.JUNIOR, self.SENIOR}
