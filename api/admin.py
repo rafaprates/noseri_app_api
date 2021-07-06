a371ee5003a058ea3fd5825c328aa9503f8941a8
Rafael <rafaelbprates@gmail.com>
2021-07-06 15:34:45 -0400

    add migration for api app

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from .models import Load, Kwh, Student, BillingInfo
+from .models import Load, Kwh, BillingInfo
 
 # Register your models here.
 # Os modelos aqui registrados aparecerão em /admin
 
 admin.site.register(Load)
 admin.site.register(Kwh)
-admin.site.register(Student)
 admin.site.register(BillingInfo)
 
 
--- api/models.py
+++ api/models.py
@@ -63,26 +63,3 @@ class BillingInfo(models.Model):
 
     def __str__(self):
         return f"Cliente: {self.user}"
-
-
-class Student(models.Model):
-    FRESHMAN = 'FR'
-    SOPHOMORE = 'SO'
-    JUNIOR = 'JR'
-    SENIOR = 'SR'
-    GRADUATE = 'GR'
-    YEAR_IN_SCHOOL_CHOICES = [
-        (FRESHMAN, 'Freshman'),
-        (SOPHOMORE, 'Sophomore'),
-        (JUNIOR, 'Junior'),
-        (SENIOR, 'Senior'),
-        (GRADUATE, 'Graduate'),
-    ]
-    year_in_school = models.CharField(
-        max_length=2,
-        choices=YEAR_IN_SCHOOL_CHOICES,
-        default=FRESHMAN,
-    )
-
-    def is_upperclass(self):
-        return self.year_in_school in {self.JUNIOR, self.SENIOR}
