253128cc71457ddac0dc7a4a1dd2464bc73694f7
Rafael <rafaelbprates@gmail.com>
2021-07-13 18:04:56 -0400

    API Load and User fields now show their names instead of their ForeignKey

--- api/models.py
+++ api/models.py
@@ -11,6 +11,7 @@ class Load(models.Model):
     """Esta tabela guarda informações a respeito das cargas."""
     load = models.CharField(max_length = 32)
 
+
     def __str__(self):
         return self.load
     
--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 from .models import Load, Kwh
 
 class KwhSerializer(serializers.ModelSerializer):
+
+    user = serializers.ReadOnlyField(source='user.username')
+    load = serializers.ReadOnlyField(source='load.load')
+
+
     class Meta:
         model = Kwh
         fields = ['user', 'load', 'kwh', 'timestamp',]
