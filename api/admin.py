05e6ea975df3a4f0681f806d479b2ca48b576b68
Rafael <rafaelbprates@gmail.com>
2021-08-07 16:44:14 -0400

    [feature] registered UserLoadAssociation to Admin site

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Load, Kwh, BillingInfo, Tax
+from api.models import Load, Kwh, BillingInfo, Tax, UserLoadAssociation
 
 # Register your models here.
 # Os modelos aqui registrados aparecerão em /admin
 
 admin.site.register(Load)
 admin.site.register(Kwh)
+admin.site.register(UserLoadAssociation)
 admin.site.register(BillingInfo)
 admin.site.register(Tax)
 
